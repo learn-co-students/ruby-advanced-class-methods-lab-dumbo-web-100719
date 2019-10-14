167a5c6c65ec1a8cf55fce23d3162ade99353fe1
robin-raq <robin.raq@gmail.com>
2019-10-13 22:42:48 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,33 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  
+  def self.new_by_name(title)
+    named_song = self.new
+    named_song.name = title
+    named_song
+    
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.create_by_name (name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  
+  end
+
+  def self.find_by_name (name)
+    @@all.find {|song| song.name == name}
+  end
 
   def self.all
     @@all
@@ -10,4 +37,39 @@ class Song
     self.class.all << self
   end
 
+  def self.find_or_create_by_name (name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(path)
+    ids = path.split("-")
+    ids_no_mp3 = ids[1].split(".")
+    song = self.new
+    song.artist_name = ids[0].strip!
+    song.name = ids_no_mp3[0].strip!
+    song
+    #binding.pry
+    
+  end
+  def self.create_from_filename(path)
+    # ids = path.split("-")
+    # ids_no_mp3 = ids[1].split(".")
+    # song = self.new
+    # song.artist_name = ids[0].strip!
+    # song.name = ids_no_mp3[0].strip!
+    song = self.new_from_filename(path)
+    song.save
+    song
+    #binding.pry
+    
+  end
+
+  def self.destroy_all
+
+    @@all = []
+  end
 end
